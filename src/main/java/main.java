44f3389192586255d98c0b1f1c67dd91f6d5b5f1
noNhead <sun495fun@gmail.com>
2021-03-17 14:48:39 +0400

    Deleted useless methods and update working methods Deleted Gson methods Created marshall and unmarshall methods Add generated POJO Updated gitignore Created new folder in resources

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,89 +1,36 @@
-import entity.Categories;
-import entity.Category;
-import entity.Product;
-import entity.Subcategory;
 import service.Service;
 
 import javax.xml.bind.JAXBException;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.transform.TransformerException;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.time.LocalDate;
-import java.util.ArrayList;
 
 public class main {
     public static void main(String[] args) {
         Service service = new Service();
 
-        /*Product product = new Product(
-                "aa111",
-                "manufacturer",
-                LocalDate.now(),
-                "#000000",
-                10000,
-                10);
-
-        Product product1 = new Product(
-                "ab111",
-                "manufacturer",
-                LocalDate.now(),
-                "#000001",
-                10000,
-                10);
-
-        Product product2 = new Product(
-                "ac111",
-                "manufacturer",
-                LocalDate.now(),
-                "#000000",
-                10000,
-                10);
-
-
-        Subcategory subcategory = new Subcategory(new ArrayList<>(), "title1");
-        Subcategory subcategory1 = new Subcategory(new ArrayList<>(), "title2");
-        Subcategory subcategory2 = new Subcategory(new ArrayList<>(), "title3");
-
-        subcategory.getProducts().add(product);
-        subcategory.getProducts().add(product1);
-        subcategory1.getProducts().add(product1);
-        subcategory2.getProducts().add(product2);
-
-        Category category = new Category(new ArrayList<>(), "title");
-        category.getSubcategories().add(subcategory);
-        category.getSubcategories().add(subcategory1);
-        category.getSubcategories().add(subcategory2);
-
-        Categories categories = new Categories(new ArrayList<>());
-        categories.getCategories().add(category);
-        categories.getCategories().add(category);
+        generated.Categories gCategories = new generated.Categories();
 
         try {
-            service.documentCreator(categories);
-        } catch (ParserConfigurationException | TransformerException | JAXBException e) {
+            gCategories = service.unmarshall();
+        } catch (JAXBException | FileNotFoundException e) {
             e.printStackTrace();
         }
 
-
-
-
         try {
-            service.documentCreator(service.documentReaderWithStax());
+            service.marshall(gCategories);
         } catch (JAXBException e) {
             e.printStackTrace();
         }
 
-
         try {
-            service.xmlToJsonConverterWithJackson();
-        } catch (IOException | XMLStreamException e) {
+            service.CategoriesToJson(gCategories);
+        } catch (IOException e) {
             e.printStackTrace();
         }
-*/
+
         try {
-            service.jsonToXmlConverter();
-        } catch (IOException e) {
+            service.marshall(service.JsonToCategories());
+        } catch (IOException | JAXBException e) {
             e.printStackTrace();
         }
     }
--- src/main/java/service/Service.java
+++ src/main/java/service/Service.java
@@ -4,38 +4,29 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
-import config.LocalDateAdapterForGson;
-import entity.Categories;
-import entity.Category;
-import entity.Product;
-import entity.Subcategory;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.stream.XMLStreamException;
 import java.io.*;
-import java.lang.reflect.Type;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collection;
 
 public class Service {
     StaxReader staxReader = new StaxReader();
 
-    public void documentCreator(Categories categories) throws JAXBException {
-        JAXBContext context = JAXBContext.newInstance(Categories.class);
+    public void marshall(generated.Categories categories) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(generated.Categories.class);
         Marshaller marshaller = context.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        marshaller.marshal(categories, new File("src/main/resources/categories.xml"));
+        marshaller.marshal(categories, new File("src/main/resources/generated/xml/categories.xml"));
     }
 
-    public Categories documentReaderWithStax() {
+    public generated.Categories unmarshall() throws JAXBException, FileNotFoundException {
+        JAXBContext context = JAXBContext.newInstance(generated.Categories.class);
+        return (generated.Categories) context.createUnmarshaller().unmarshal(new FileReader("src/main/resources/categories.xml"));
+    }
+
+    public generated.Categories documentReaderWithStax() {
         try {
             return staxReader.staxReader();
         } catch (FileNotFoundException | XMLStreamException e) {
@@ -44,32 +35,20 @@ public class Service {
         return null;
     }
 
-    public void jsonToXmlConverter() throws IOException {
-        ObjectMapper jsonMapper = new ObjectMapper();
-        Categories categories = jsonMapper.readValue(new String(Files.readAllBytes(Paths.get("src/main/resources/categories.json"))), Categories.class);
-        System.out.println(categories.getCategories().size());
-    }
-
-    public void jsonToXmlConverterWithGson() throws IOException {
-        Gson gson = new Gson();
-        System.out.println(new String(Files.readAllBytes(Paths.get("src/main/resources/categories.json"))));
-        Type categoryType = new TypeToken<Collection<Category>>(){}.getType();
-        Type subcategoryType = new TypeToken<Collection<Subcategory>>(){}.getType();
-        Type productType = new TypeToken<Collection<Product>>(){}.getType();
-        //gson.adapter
-
-        Categories categories = gson.fromJson(new String(Files.readAllBytes(Paths.get("src/main/resources/categories.json"))), Categories.class);
-        System.out.println(categories.getCategories().size());
-    }
-
-    public void xmlToJsonConverterWithJackson() throws IOException, XMLStreamException {
-        Categories categories = staxReader.staxReader();
-        System.out.println(categories.getCategories().size());
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        mapper.configure(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS, false);
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        mapper.writeValue(new File("src/main/resources/categories.json"), categories);
+    public void CategoriesToJson(generated.Categories categories) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS, false);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.writeValue(new File("src/main/resources/generated/json/categories.json"), categories);
 
     }
+
+    public generated.Categories JsonToCategories() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return objectMapper.readValue(new File("src/main/resources/generated/json/categories.json"), generated.Categories.class);
+    }
+
 }
